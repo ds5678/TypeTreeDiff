cb7f89f8a8d60aff231de6eb42fb242fddb57a3e
Jeremy Pritts <jrpritts7@gmail.com>
2021-09-10 04:00:38 -0400

    new console project

---
+++ TypeTreeDiffConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TypeTreeDiffConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+    }
+}
